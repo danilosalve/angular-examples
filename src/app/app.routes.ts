6049fb078b3fda31e18c688eeb22b957ab029948
Danilo Otavio Lima Salve <danilo.salve@totvs.com.br>
2025-01-28 14:53:23 -0300

    feat(not-found): adicionado tratamento para recurso não encontrado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,13 @@ export const APP_ROUTES: Routes = [
   {
     path: '',
     loadChildren: () => import('./pages/home/home.routes').then(r => r.HOME_ROUTES)
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found'
+  },
+  {
+    path: 'not-found',
+    loadComponent: () => import('./core/errors/not-found/not-found.component').then(c => c.NotFoundComponent)
   }
 ];
---
+++ src/app/core/errors/not-found/not-found.component.ts
@@ -0,0 +1,18 @@
+import { NgOptimizedImage } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { PoButtonModule, PoContainerModule } from '@po-ui/ng-components';
+
+@Component({
+  selector: 'app-not-found',
+  imports: [PoContainerModule, PoButtonModule, NgOptimizedImage],
+  templateUrl: './not-found.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NotFoundComponent {
+  private readonly router: Router = inject(Router);
+
+  navigateToHome(): void {
+    this.router.navigate([''])
+  }
+}
