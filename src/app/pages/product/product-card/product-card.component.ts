8b31b87a7cc7bd8e8143ceecfb642fb52bba6023
Danilo Otavio Lima Salve <danilo.salve@totvs.com.br>
2025-02-07 17:23:25 -0300

    feat(rating): adicionar componente de rating

--- src/app/pages/product/product-card/product-card.component.ts
+++ src/app/pages/product/product-card/product-card.component.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { PoAvatarModule, PoInfoModule, PoTagModule, PoWidgetModule } from '@po-ui/ng-components';
 
 import { Product } from '../shared/interfaces/product';
+import { RatingComponent } from '../../../shared/components/rating/rating.component';
 
 @Component({
   selector: 'app-product-card',
-  imports: [PoWidgetModule, PoInfoModule, PoTagModule, PoAvatarModule],
+  imports: [PoWidgetModule, PoInfoModule, PoTagModule, PoAvatarModule, RatingComponent],
   templateUrl: './product-card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
---
+++ src/app/shared/components/rating/rating.component.ts
@@ -0,0 +1,24 @@
+import { Component, input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rating',
+  imports: [],
+  templateUrl: './rating.component.html',
+})
+export class RatingComponent implements OnInit {
+  rating = input.required<number>();
+  max = input.required<number>();
+  count = input<number>(0);
+  stars: number[] = [];
+
+  ngOnInit(): void {
+    for (let index = 0; index < this.max(); index++) {
+      this.stars.push(index);
+    }
+  }
+
+  getStarClass(index: number): string {
+    if (index <= this.rating()) return 'an-fill an-star';
+    return index - 1 + 0.5 < this.rating() ? 'an-fill an-star-half' : 'an an-star';
+  }
+}
