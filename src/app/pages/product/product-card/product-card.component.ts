317b1154251113edd6179d302ee67368bbde6608
Danilo Otavio Lima Salve <danilo.salve@totvs.com.br>
2025-02-07 17:45:27 -0300

    fix(layout): correção de layout da pagina de produtos para smartphone

--- src/app/pages/product/product-card/product-card.component.ts
+++ src/app/pages/product/product-card/product-card.component.ts
@@ -1,15 +1,27 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, OnInit } from '@angular/core';
 import { PoAvatarModule, PoInfoModule, PoTagModule, PoWidgetModule } from '@po-ui/ng-components';
 
 import { Product } from '../shared/interfaces/product';
 import { RatingComponent } from '../../../shared/components/rating/rating.component';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-card',
-  imports: [PoWidgetModule, PoInfoModule, PoTagModule, PoAvatarModule, RatingComponent],
+  imports: [PoWidgetModule, PoInfoModule, PoTagModule, PoAvatarModule, RatingComponent, CurrencyPipe],
   templateUrl: './product-card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductCardComponent {
+export class ProductCardComponent implements OnInit {
   product = input.required<Product>();
+  height = 250;
+
+  ngOnInit(): void {
+    if (this.isSmartPhone())  {
+      this.height = 420;
+    }
+  }
+
+  isSmartPhone(): boolean {
+    return window.innerWidth < 576;
+  }
 }
