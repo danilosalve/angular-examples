510672b7a04ddfcc8473d3a56b7bcb7878b83f0f
Danilo Otavio Lima Salve <danilo.salve@totvs.com.br>
2025-02-11 10:09:31 -0300

    feat(product): adicionar campos de pesquisa

--- src/app/pages/product/products/products.component.ts
+++ src/app/pages/product/products/products.component.ts
@@ -4,19 +4,32 @@ import {
   PoFieldModule,
   PoLoadingModule,
   PoPageModule,
+  PoSearchModule,
   PoSelectComponent,
   PoSelectOption,
 } from '@po-ui/ng-components';
 import { ProductCardComponent } from '../product-card/product-card.component';
 import { ProductsService } from '../shared/services/products.service';
 import { FormsModule } from '@angular/forms';
+import { ProductsSearchPipe } from '../shared/pipe/products-search.pipe';
 //
 @Component({
   selector: 'app-products',
-  imports: [PoPageModule, PoLoadingModule, PoContainerModule, PoFieldModule, ProductCardComponent, FormsModule],
+  imports: [
+    PoPageModule,
+    PoLoadingModule,
+    PoContainerModule,
+    PoFieldModule,
+    PoSearchModule,
+    ProductCardComponent,
+    FormsModule,
+    ProductsSearchPipe,
+  ],
   templateUrl: './products.component.html',
 })
 export class ProductsComponent implements OnInit {
+  readonly filter = signal<string>('');
+  readonly filterKeys: string[] = ['title', 'category'];
   readonly limit = signal<number>(10);
   readonly poSelect = viewChild.required(PoSelectComponent);
   readonly productsService = inject(ProductsService);
@@ -43,5 +56,9 @@ export class ProductsComponent implements OnInit {
   onChangeLimit(limit: number): void {
     this.limit.set(limit);
     this.productsService.updateLimit(this.limit());
+  }
+
+  onChangeFilter($event: { filter: string[] | string; value: string }): void {
+    this.filter.set($event.value);
   }
 }
---
+++ src/app/pages/product/shared/pipe/products-search.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Product } from '../interfaces/product';
+
+@Pipe({
+  name: 'productsSearch',
+})
+export class ProductsSearchPipe implements PipeTransform {
+  transform(products: Product[], filter: string): Product[] | undefined {
+    if (filter) {
+      return products.filter(
+        (p: Product) =>
+          p.title.toLocaleLowerCase().startsWith(filter.toLocaleLowerCase()) ||
+          p.category.toLocaleLowerCase().startsWith(filter.toLocaleLowerCase())
+      );
+    } else {
+      return products;
+    }
+  }
+}
