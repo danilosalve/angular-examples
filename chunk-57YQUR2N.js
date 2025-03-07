ad11c4a686708781f50ce6d42d0bc8ad0c7c91c8
Danilo Otavio Lima Salve <danilo.salve@totvs.com.br>
2025-03-07 12:02:40 -0300

    Auto-generated commit

--- chunk-57YQUR2N.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-4CLCTAJ7.js";var t=[{path:"",title:"Produtos",loadComponent:()=>import("./chunk-GBSAXA4J.js").then(o=>o.ProductsComponent)}];export{t as PRODUCT_ROUTES};
---
+++ chunk-ENEYFBVI.js
@@ -0,0 +1,1 @@
+import"./chunk-4CLCTAJ7.js";var t=[{path:"",title:"Produtos",loadComponent:()=>import("./chunk-TUYUGEHK.js").then(o=>o.ProductsComponent)}];export{t as PRODUCT_ROUTES};
