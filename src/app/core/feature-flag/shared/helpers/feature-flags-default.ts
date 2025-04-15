7913e1c8ec991b02c702f51262b2bd6f14af4184
Danilo Otavio Lima Salve <danilo.salve@totvs.com.br>
2025-04-15 08:46:13 -0300

    fix(feature-flags): alterar URL da API de Flags e adicionar um valor default em caso de falha no servidor

---
+++ src/app/core/feature-flag/shared/helpers/feature-flags-default.ts
@@ -0,0 +1,14 @@
+import { FeatureFlag } from "../interfaces/feature-flag";
+
+/**
+ * Constante com valores padrões das Flags
+ *
+ * @useBy Utilizado caso o servidor esteja fora do ar.
+ */
+export const DEFAULT_FLAGS: FeatureFlag = {
+  featureFlags: {
+    users: false,
+    darkMode: true,
+    isAdmin: true
+  }
+}
--- src/app/core/feature-flag/shared/services/feature-flag.service.ts
+++ src/app/core/feature-flag/shared/services/feature-flag.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { catchError, Observable, of, tap } from 'rxjs';
 
 import { FeatureFlag } from '../interfaces/feature-flag';
 import { FeatureFlagStoreService } from './feature-flag-store.service';
+import { DEFAULT_FLAGS } from '../helpers/feature-flags-default';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +13,7 @@ export class FeatureFlagService {
   private readonly http = inject(HttpClient);
   private readonly featureFlagStore = inject(FeatureFlagStoreService);
   /* API fake gerada em https://designer.mocky.io/design -> Não é possivel modificar o valor do registro */
-  private readonly apiUrl = 'https://run.mocky.io/v3/76d1912a-9b9b-4fd0-8db8-e5091382f6f6';
+  private readonly apiUrl = 'https://run.mocky.io/v3/8bf14630-5c64-4490-91cf-84e54cf204d0';
 
   getFlags(): Observable<FeatureFlag> {
     const headers: HttpHeaders = new HttpHeaders({
@@ -20,6 +21,9 @@ export class FeatureFlagService {
     });
     return this.http
       .get<FeatureFlag>(this.apiUrl, { headers })
-      .pipe(tap(featureFlags => this.featureFlagStore.setStore(featureFlags)));
+      .pipe(
+        catchError(() => of(DEFAULT_FLAGS)),
+        tap(featureFlags => this.featureFlagStore.setStore(featureFlags)));
   }
 }
+
