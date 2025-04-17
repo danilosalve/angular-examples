cd35f0462c13e120bd44c5fca5b9316423fe5394
Danilo Otavio Lima Salve <danilo.salve@totvs.com.br>
2025-04-17 16:15:14 -0300

    fix(effect): requisições disparadas indevidamentes ao alterar cep

--- src/app/pages/signals/signals-sample/effect/effect.component.ts
+++ src/app/pages/signals/signals-sample/effect/effect.component.ts
@@ -1,5 +1,5 @@
 import { FormsModule } from '@angular/forms';
-import { Component, effect, inject, isDevMode, resource, signal } from '@angular/core';
+import { Component, computed, effect, inject, isDevMode, resource, signal } from '@angular/core';
 import {
   PoContainerModule,
   PoFieldModule,
@@ -20,13 +20,16 @@ export class EffectComponent {
   private readonly apiUrl = isDevMode() ? 'http://viacep.com.br/ws' : 'https://viacep.com.br/ws';
   zipCode = signal<string>('');
   enabled = signal(false);
+  currentZipCode = computed(() => (this.zipCode().length === 8 ? this.zipCode() : this.lastZipCode()));
+  lastZipCode = signal<string>('');
 
   addressResource = resource({
-    request: this.zipCode,
+    request: this.currentZipCode,
     loader: ({ request: zipCode, abortSignal }): Promise<Address> => {
       return fetch(`${this.apiUrl}/${zipCode || '0'}/json/`, { signal: abortSignal }).then(
         address => address.json() as unknown as Address
       );
+      // return Promise.resolve(ADDRESS_DEFAULT)
     },
   });
 
@@ -41,6 +44,7 @@ export class EffectComponent {
     this.enabled.set(isEnable);
 
     if (isEnable) {
+      this.lastZipCode.set(this.zipCode());
       this.notification.information(`Buscando o CEP ${this.zipCode()}...`);
     }
   }
