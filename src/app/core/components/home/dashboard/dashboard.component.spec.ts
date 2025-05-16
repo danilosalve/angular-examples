e9d661fa3fc6e3387a3f5656a9b69d0693593311
Danilo Otavio Lima Salve <danilo.salve@totvs.com.br>
2025-05-16 17:36:20 -0300

    tests(jest): substitui karma/jasmine pelo jest

---
+++ src/app/core/components/home/dashboard/dashboard.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DashboardComponent } from './dashboard.component';
+
+describe('DashboardComponent', () => {
+  let component: DashboardComponent;
+  let fixture: ComponentFixture<DashboardComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [DashboardComponent]
+    });
+    fixture = TestBed.createComponent(DashboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('deve ser instanciado', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('deve exibir o título principal correto', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('po-page-default').textContent).toContain('Página Inicial');
+  });
+});
