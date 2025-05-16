fc2e82569e449a62b3b0de98b527be4647f99c80
Danilo Otavio Lima Salve <danilo.salve@totvs.com.br>
2025-05-16 17:36:20 -0300

    tests(jest): substitui karma/jasmine pelo jest

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+// jest.config.js
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest', // Only transform .ts files
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!flat)/', // Exclude modules except 'flat' from transformation
+  ],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': '<rootDir>/style-mock.js'
+  },
+};
