b7bff771321b58335d11403213b45563613d85b3
Danilo Otavio Lima Salve <danilo.salve@totvs.com.br>
2025-05-20 09:37:35 -0300

    test(config): move arquivos de mock do jest para pasta correta

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,8 @@ module.exports = {
     '/node_modules/(?!flat)/', // Exclude modules except 'flat' from transformation
   ],
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': '<rootDir>/style-mock.js'
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/file-mock.js',
+    '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/style-mock.js'
   },
 };
