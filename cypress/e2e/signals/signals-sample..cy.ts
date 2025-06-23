b84de861f520a2e3b7c7b4a271373cab21272e14
Danilo Otavio Lima Salve <danilo.salve@totvs.com.br>
2025-06-23 18:25:51 -0300

    feat(ci): adiciona conteiner para execução dos testes

---
+++ cypress/e2e/signals/signals-sample..cy.ts
@@ -0,0 +1,81 @@
+describe('signals-sample', () => {
+  beforeEach(() => cy.visit('/signals/samples'));
+
+  context('Untracked', () => {
+    it('deve garantir que o campo nome está vazio ao carregar', () => {
+      cy.get('[formcontrolname="name"]').should('exist').should('have.value', '');
+      cy.get('p.po-mb-1.po-sm-12').should(
+        'contain.text',
+        'O nome agora é Nome não informado e o valor do contador era 0 quando o nome mudou'
+      );
+    });
+
+    it('deve preencher o campo Nome sem incrementar o contador', () => {
+      cy.get('[formControlName="name"]').type('Danilo');
+      cy.get('p.po-mb-1.po-sm-12').should(
+        'contain.text',
+        'O nome agora é Danilo e o valor do contador era 0 quando o nome mudou'
+      );
+      cy.get('p.po-sm-12').should('contain.text', 'Contador Atual: 0');
+    });
+
+    it('deve incrementar o contador sem preencher o campo Nome ', () => {
+      cy.contains('Incrementar Contador').click();
+      cy.get('p.po-mb-1.po-sm-12').should(
+        'contain.text',
+        'O nome agora é Nome não informado e o valor do contador era 0 quando o nome mudou'
+      );
+      cy.get('p.po-sm-12').should('contain.text', 'Contador Atual: 1');
+    });
+
+    it('deve incrementar o contador 2x e preencher o campo Nome', () => {
+      cy.contains('Incrementar Contador').click();
+      cy.contains('Incrementar Contador').click();
+
+      cy.get('[formControlName="name"]').type('Eduardo');
+      cy.get('p.po-mb-1.po-sm-12').should(
+        'contain.text',
+        'O nome agora é Eduardo e o valor do contador era 2 quando o nome mudou'
+      );
+      cy.get('p.po-sm-12').should('contain.text', 'Contador Atual: 2');
+    });
+  });
+
+  context('Linked', () => {
+    it('deve adicionar um curso no carinho', () => {
+      cy.get('[name="courses"] > po-field-container > div.po-field-container > div.po-field-container-content')
+        .should('exist')
+        .find('select.po-select.po-select-phosphor')
+        .select(3);
+
+      cy.get('[name="quantity"]').should('exist').find('input.po-input').should('have.value', '20');
+
+      cy.contains('Adicionar ao carrinho').click();
+
+      cy.get('.po-toaster-message').should('be.visible');
+    });
+  });
+
+  context('Effect', () => {
+    it('deve buscar por um CEP', () => {
+      cy.intercept('GET', 'http://viacep.com.br/ws/0/json/').as('stubGet');
+      cy.get('input[name="zipCode"]').type('01311200');
+      cy.wait('@stubGet');
+
+      cy.get('po-loading-overlay', { timeout: 10000 }).should('not.exist');
+
+      cy.get('po-info[p-label="Logradouro"]').should('exist')
+      cy.get('po-info[ng-reflect-value="Avenida Paulista"]').should('exist');
+      cy.get('po-info[p-label="Bairro"]').should('exist');
+      cy.get('po-info[ng-reflect-value="Bela Vista"]').should('exist');
+      cy.get('po-info[p-label="Cidade"]').should('exist');
+      cy.get('po-info[ng-reflect-value="São Paulo"]').should('exist');
+      cy.get('po-info[p-label="Estado"]').should('exist');
+      cy.get('po-info[ng-reflect-value="São Paulo"]').should('exist');
+      cy.get('po-info[p-label="Região"]').should('exist');
+      cy.get('po-info[ng-reflect-value="Sudeste"]').should('exist');
+      cy.get('po-info[p-label="DDD"]').should('exist');
+      cy.get('po-info[ng-reflect-value="11"]').should('exist');
+    });
+  });
+});
