7e24f8138436ae80e4fcb6d0633ec5fe0a0d8f5a
Danilo Otavio Lima Salve <danilo.salve@totvs.com.br>
2025-06-27 14:00:02 -0300

    feat(typelist): adiciona componente para selecão tipo de visualização de dados

---
+++ src/app/shared/components/type-list-button/type-list-button.component.ts
@@ -0,0 +1,22 @@
+import { DeviceService } from './../../services/device.service';
+import { Component, computed, inject, output, signal } from '@angular/core';
+import { PoButtonModule } from '@po-ui/ng-components';
+
+@Component({
+  selector: 'app-type-list-button',
+  imports: [PoButtonModule],
+  templateUrl: './type-list-button.component.html'
+})
+export class TypeListButtonComponent {
+  /** Se tipo de visualização é tabela retorna verdadeiro*/
+  readonly changeTypeView = output<boolean>();
+  readonly deviceService = inject(DeviceService);
+  readonly isTableView = signal(!this.deviceService.isSmartPhone());
+  readonly icon = computed(() => this.isTableView() ? 'an an-list' : 'an an-cards');
+  readonly label = computed(() => this.isTableView() ? 'Tabela' : 'Lista');
+
+  onChangeTypeView(): void {
+    this.isTableView.update(value => !value);
+    this.changeTypeView.emit(this.isTableView());
+  }
+}
