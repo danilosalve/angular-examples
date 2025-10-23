319c614ce0eeffd1a36d7c72fa83f8f1bbaea6a2
Danilo Otavio Lima Salve <danilo.salve@totvs.com.br>
2025-10-23 13:35:21 -0300

    tests(e2e): altera label com quantidade de pokemons

--- cypress/e2e/pokedex.cy.ts
+++ cypress/e2e/pokedex.cy.ts
@@ -12,7 +12,7 @@ describe('pokedex', () => {
       cy.get('.po-page-header-title').should('be.visible');
       cy.get('.po-page-header-title').should('contain.text', 'Pokedex');
 
-      cy.get('[data-cy="app-results"]').should('contain.text', 'Exibindo: 1 - 12 de 1302');
+      cy.get('[data-cy="app-results"]').should('contain.text', 'Exibindo: 1 - 12 de 1328');
     });
 
     it('deve obter um erro ao tentar listar os pokemons', () => {
