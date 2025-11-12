7d6a443ee31fb6df7157ac3e5e7aa6a6275cce12
Danilo Otavio Lima Salve <danilo.salve@totvs.com.br>
2025-11-12 11:33:25 -0300

    Auto-generated commit

--- chunk-34DHTEDA.js
+++
@@ -1,1 +0,0 @@
-import{D as o}from"./chunk-ZOMRHQQG.js";import{ia as t}from"./chunk-TTU3ZVAH.js";var r=class{constructor(){this.router=t(o)}navigateToHome(){this.router.navigate([""])}};export{r as a};
---
+++ chunk-LMMNOUTZ.js
@@ -0,0 +1,1 @@
+import{D as o}from"./chunk-SBPM3P26.js";import{ia as t}from"./chunk-ROBB4PN5.js";var r=class{constructor(){this.router=t(o)}navigateToHome(){this.router.navigate([""])}};export{r as a};
