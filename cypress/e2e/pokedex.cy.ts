890f1662c166aec98368c9131d6093981a7bbaf3
Danilo Otavio Lima Salve <danilo.salve@totvs.com.br>
2025-12-23 09:13:13 -0300

    test(pokemon): altera valor da label de resultado por pagina

--- cypress/e2e/pokedex.cy.ts
+++ cypress/e2e/pokedex.cy.ts
@@ -12,7 +12,7 @@ describe('pokedex', () => {
       cy.get('.po-page-header-title').should('be.visible');
       cy.get('.po-page-header-title').should('contain.text', 'Pokedex');
 
-      cy.get('[data-cy="app-results"]').should('contain.text', 'Exibindo: 1 - 12 de 1328');
+      cy.get('[data-cy="app-results"]').should('contain.text', 'Exibindo: 1 - 12 de 1350');
     });
 
     it('deve obter um erro ao tentar listar os pokemons', () => {
