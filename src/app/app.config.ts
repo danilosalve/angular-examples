c92647977097f50d15990c403f202fb7ea33d0d7
Danilo Otavio Lima Salve <danilo.salve@totvs.com.br>
2026-01-20 14:37:27 -0300

    feat(in-memory): adiciona exemplo de uso do Angular InMemoryAPI

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,17 +4,28 @@ import { provideHttpClient } from '@angular/common/http';
 import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
 import { PoHttpRequestModule } from '@po-ui/ng-components';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgEventBus } from 'ng-event-bus';
 
 import { APP_ROUTES } from './app.routes';
 import { loadFeatureFlagsFn } from './core/feature-flag/shared/helpers/loadFeatureFlags';
-import { NgEventBus } from 'ng-event-bus';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(APP_ROUTES),
     provideHttpClient(),
     provideAnimations(),
-    importProvidersFrom([BrowserAnimationsModule, PoHttpRequestModule, BrowserModule]),
+    importProvidersFrom([
+      BrowserAnimationsModule,
+      PoHttpRequestModule,
+      BrowserModule,
+      InMemoryWebApiModule.forRoot(InMemoryDataService, {
+        delay: 2000,
+        dataEncapsulation: false,
+        passThruUnknownUrl: true // permite que URLs não definidas sigam para o backend real
+      })
+    ]),
     provideAppInitializer(loadFeatureFlagsFn),
     NgEventBus
   ]
